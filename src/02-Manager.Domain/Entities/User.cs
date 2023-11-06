5ab7f849bfd5680fad3e5b855eee39e8a52fc935
Thiagoferlima <th1gox2003@gmail.com>
2023-11-05 23:20:55 -0300

    camada Infra\Domain

---
+++ src/02-Manager.Domain/Entities/User.cs
@@ -0,0 +1,57 @@
+﻿using Manager.Domain.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manager.Core.Exceptions;
+namespace Manager.Domain.Entities
+{
+    public class User : Base 
+    {
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+        //EF//
+        protected User() { }
+        public User(string name, string email, string password)
+        {
+            Name=name;
+            Email=email;
+            Password=password;
+            _errors = new List<string>();
+        }
+        public void ChangeNamw(string name) 
+        {
+            Name = name;
+            Validate();
+        }
+        public void ChangePassword(string password) 
+        {
+            Password = password;
+            Validate();
+        }
+        public void ChangeEmail() 
+        {
+            Email = Email;
+            Validate();
+        }
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if(!validation.IsValid) 
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+
+                throw new DomainException("Alguns campos estão inválidos!",  _errors);
+            }
+            return true;
+        }
+
+    }
+}
